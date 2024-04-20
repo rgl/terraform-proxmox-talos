fb664c89a0aaa17439b7edbbf9177ec923710c71
Rui Lopes <rgl@ruilopes.com>
2024-04-20 08:07:02 +0100

    upgrade the terraform providers

--- providers.tf
+++ providers.tf
@@ -6,7 +6,7 @@ terraform {
     # see https://github.com/hashicorp/terraform-provider-random
     random = {
       source  = "hashicorp/random"
-      version = "3.6.0"
+      version = "3.6.1"
     }
     # see https://registry.terraform.io/providers/hashicorp/cloudinit
     # see https://github.com/hashicorp/terraform-provider-cloudinit
@@ -18,19 +18,19 @@ terraform {
     # see https://github.com/bpg/terraform-provider-proxmox
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.51.1"
+      version = "0.54.0"
     }
     # see https://registry.terraform.io/providers/siderolabs/talos
     # see https://github.com/siderolabs/terraform-provider-talos
     talos = {
       source  = "siderolabs/talos"
-      version = "0.4.0"
+      version = "0.5.0"
     }
     # see https://registry.terraform.io/providers/hashicorp/helm
     # see https://github.com/hashicorp/terraform-provider-helm
     helm = {
       source  = "hashicorp/helm"
-      version = "2.13.0"
+      version = "2.13.1"
     }
   }
 }
