10078bb82d0977517b8f78a2ef96c8ca78555aae
Rui Lopes <rgl@ruilopes.com>
2024-05-03 20:03:23 +0100

    upgrade to talos 1.7.1

--- variables.tf
+++ variables.tf
@@ -8,11 +8,11 @@ variable "proxmox_pve_node_address" {
 }
 
 # see https://github.com/siderolabs/talos/releases
-# see https://www.talos.dev/v1.6/introduction/support-matrix/
+# see https://www.talos.dev/v1.7/introduction/support-matrix/
 variable "talos_version" {
   type = string
   # renovate: datasource=github-releases depName=siderolabs/talos
-  default = "1.6.7"
+  default = "1.7.1"
   validation {
     condition     = can(regex("^\\d+(\\.\\d+)+", var.talos_version))
     error_message = "Must be a version number."
@@ -20,7 +20,7 @@ variable "talos_version" {
 }
 
 # see https://github.com/siderolabs/kubelet/pkgs/container/kubelet
-# see https://www.talos.dev/v1.6/introduction/support-matrix/
+# see https://www.talos.dev/v1.7/introduction/support-matrix/
 variable "kubernetes_version" {
   type = string
   # renovate: datasource=github-releases depName=siderolabs/kubelet
