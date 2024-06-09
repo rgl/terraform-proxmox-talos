94bd153ce07ae38e6a35cd06a203f9115f84694f
Rui Lopes <rgl@ruilopes.com>
2024-06-09 10:28:41 +0100

    upgrade to talos 1.7.4

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "proxmox_pve_node_address" {
 variable "talos_version" {
   type = string
   # renovate: datasource=github-releases depName=siderolabs/talos
-  default = "1.7.2"
+  default = "1.7.4"
   validation {
     condition     = can(regex("^\\d+(\\.\\d+)+", var.talos_version))
     error_message = "Must be a version number."
