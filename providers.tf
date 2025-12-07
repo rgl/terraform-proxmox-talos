ef9121d52cc3ed9e080eb11a2af42f41d8c59b9f
Rui Lopes <rgl@ruilopes.com>
2025-12-07 07:23:54 +0000

    upgrade to bpg/proxmox 0.88.0

--- providers.tf
+++ providers.tf
@@ -18,7 +18,7 @@ terraform {
     # see https://github.com/bpg/terraform-provider-proxmox
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.84.1"
+      version = "0.88.0"
     }
     # see https://registry.terraform.io/providers/siderolabs/talos
     # see https://github.com/siderolabs/terraform-provider-talos
