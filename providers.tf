a0954f11c6a2bf32c6a3d7cd3d48fa7612928e62
Rui Lopes <rgl@ruilopes.com>
2026-02-01 15:20:00 +0000

    upgrade to hashicorp/random 3.8.1

--- providers.tf
+++ providers.tf
@@ -6,7 +6,7 @@ terraform {
     # see https://github.com/hashicorp/terraform-provider-random
     random = {
       source  = "hashicorp/random"
-      version = "3.8.0"
+      version = "3.8.1"
     }
     # see https://registry.terraform.io/providers/hashicorp/cloudinit
     # see https://github.com/hashicorp/terraform-provider-cloudinit
