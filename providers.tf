cba22741d886906539e728189235f35015d8b173
Rui Lopes <rgl@ruilopes.com>
2026-02-01 15:20:00 +0000

    upgrade to bpg/proxmox 0.93.1

--- providers.tf
+++ providers.tf
@@ -18,7 +18,7 @@ terraform {
     # see https://github.com/bpg/terraform-provider-proxmox
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.93.0"
+      version = "0.93.1"
     }
     # see https://registry.terraform.io/providers/siderolabs/talos
     # see https://github.com/siderolabs/terraform-provider-talos
